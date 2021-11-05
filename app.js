c13345d28ae2f5399916232116e4e52d2b93dabd
Mohamed <M.Mahgoub04@gmail.com>
2021-11-06 01:51:18 +0400

    Implemented themes, added Red and Green themes

--- app.js
+++ app.js
@@ -1,8 +1,60 @@
+function bluetheme() {
+  document.querySelectorAll('button').forEach(x => {
+    x.style.backgroundColor = '#90e0ef'
+    x.style.color = "#14213d"
+    x.style.border = '1px solid #14213d'
+    x.onmouseover = () => {
+      x.style.backgroundColor = '#1DBBEC'
+    }
+    x.onmouseout = () => {
+      x.style.backgroundColor = '#90e0ef'
+    }
+  })
+  document.querySelector('main').style.backgroundColor = '#14213d'
+  document.querySelector('#display').style.backgroundColor = '#90e0ef'
+  document.querySelector('h5').style.color = '#14213d'
+  document.querySelector('h3').style.color = '#14213d'
+}
+function redtheme() {
+  document.querySelectorAll('button').forEach(x => {
+    x.style.backgroundColor = '#EF7598'
+    x.style.color = "#641220"
+    x.style.border = '1px solid #641220'
+    x.onmouseover = () => {
+      x.style.backgroundColor = '#da1e37'
+    }
+    x.onmouseout = () => {
+      x.style.backgroundColor = '#EF7598'
+    }
+  })
+  document.querySelector('main').style.backgroundColor = '#641220'
+  document.querySelector('#display').style.backgroundColor = '#EF7598'
+  document.querySelector('h5').style.color = '#641220'
+  document.querySelector('h3').style.color = '#641220'
+}
+function greentheme() {
+  document.querySelectorAll('button').forEach(x => {
+    x.style.backgroundColor = '#83FFBD'
+    x.style.color = "#004b23"
+    x.style.border = '1px solid #004b23'
+    x.onmouseover = () => {
+      x.style.backgroundColor = '#00CF61'
+    }
+    x.onmouseout = () => {
+      x.style.backgroundColor = '#83FFBD'
+    }
+  })
+  document.querySelector('main').style.backgroundColor = '#004b23'
+  document.querySelector('#display').style.backgroundColor = '#83FFBD'
+  document.querySelector('h5').style.color = '#004b23'
+  document.querySelector('h3').style.color = '#004b23'
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 //  Declaring variables and constants
  const h3 = document.querySelector('h3')
  const h5 = document.querySelector('h5')
+ const slt = document.querySelector('select')
  let expression = ''
 
  // Clear button
@@ -45,4 +97,25 @@ document.addEventListener('DOMContentLoaded', () => {
   h5.append('=')
  }
 
+ // Theme
+ 
+ slt.onchange = () => {
+  if (slt.value == 'red'){
+    redtheme()
+  } else if (slt.value == 'blue') {
+    bluetheme()
+  } else if (slt.value == 'green') {
+    greentheme()
+  }
+
+  // switch (slt.value) {
+  //   case 'blue':
+  //     console.log('bluu')
+  //     bluetheme()
+  //   case 'red':
+  //     redtheme()
+
+  // }
+
+ }
 })
