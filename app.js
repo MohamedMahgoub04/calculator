aad48215aad8e4a772c1965f77a49c7b66794479
Mohamed <M.Mahgoub04@gmail.com>
2021-11-05 18:47:11 +0400

    Added eval(), the calculator now works

--- app.js
+++ app.js
@@ -27,67 +27,44 @@ document.addEventListener('DOMContentLoaded', () => {
  let h3 = document.querySelector('h3')
  let h5 = document.querySelector('h5')
  let operators = ['+', '-', '*', '/']
+ let expression = ''
 
  // Clear button
  document.querySelector('button').onclick = () => {
   h3.innerHTML = ''
   h5.innerHTML = ''
+  expression = ''
  }
 
  // Delete button
  document.querySelector('#delete').onclick = () => {
   h5.innerHTML = h5.innerHTML.slice(0, -1) 
+  expression = expression.slice(0, -1)
  }
 
  
  // Numbers
  document.querySelectorAll('.numbers').forEach(x => {
   x.onclick = () => {
+   expression += x.innerHTML
    h5.append(x.innerHTML)
-   let a = parseInt(x.innerHTML)
-   // console.log(parseInt(x.innerHTML))
-
-   let li = [] 
-   for (let i = 0; i < h5.innerHTML.length; i++) {
-    li.push(h5.innerHTML[i])
-   }   
-   console.log(li)
-   for (let i of ['+','-','*','/']) {
-    console.log(i)
-    if (i in li) {
-     
-     console.log(li.indexOf(i))
-    }
-   }
-   
-
   }
  })
 
  // Operands
  document.querySelectorAll('.operands').forEach(x => {
   x.onclick = () => {
-
-   let li = [] 
-   for (let i = 0; i < h5.innerHTML.length; i++) {
-    li.push(h5.innerHTML[i])
-   }   
-   console.log(li)
-   
+   expression += x.getAttribute('data-op')
    h5.append(x.innerHTML)
-   // console.log(x.innerhtml)
-
-   }
-
-   // switch (x.getAttribute('data-op')) {
-   //  case '+':
-   //   add(a, )
-   // }
-
-
-   }
-  
-
+  }
+ }
  )
+
+ document.querySelector('#equal').onclick = () => {
+  console.log(expression)
+  let answer = eval(expression)
+  console.log(answer)
+  document.querySelector('h3').innerHTML = answer
+ }
 
 })
